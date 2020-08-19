f87c841e97b3ba507bc7f598cbf352e56ba8c2f8
Deepika Pathak <deepikapathak228750@gmail.com>
2020-08-19 20:55:36 +0530

    changed title

--- teacher.php
+++ teacher.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>ALUMNI</title>
+    <title>Baba Ramdas Memorial Sr. Sec. School</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
